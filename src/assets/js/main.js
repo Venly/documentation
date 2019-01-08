24b4f30d1dff19f0f6cd4a4746b4612b50126e4c
Steve Maris <steve_maris@hotmail.com>
2019-01-08 15:05:14 +0100

    Update styling

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -2,9 +2,11 @@ $(function() {
   hljs.initHighlighting();
 
   if(window.innerWidth >= 1024) {
-    var sticky = new Waypoint.Sticky({
-      element: $('#toc')[0],
-      wrapper: false
-    });
+    if(Waypoint) {
+      var sticky = new Waypoint.Sticky({
+        element: $('#toc')[0],
+        wrapper: false
+      });
+    }
   }
 });

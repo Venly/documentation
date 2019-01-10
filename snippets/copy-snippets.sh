a7a28dba32e498fca94928c6326c90397cdbd121
davyvanroy <davy.van.roy@gmail.com>
2019-01-10 11:10:41 +0100

    also remove null pincodes

--- snippets/copy-snippets.sh
+++ snippets/copy-snippets.sh
@@ -2,6 +2,6 @@
 
 cp -r $1/* ./
 
-sed -i'.original' -e 's/ \{1,\}"pincode" : ".\{1,\}//g' -e '/^$/d' ./**/*.adoc
+sed -i'.original' -e 's/ \{1,\}"pincode" : .\{1,\}//g' -e '/^$/d' ./**/*.adoc
 
 rm -rf ./**/*.original

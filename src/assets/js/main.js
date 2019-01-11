59970daefcdc96fcc9fa584a78c5db9ac9e93436
Steve Maris <steve_maris@hotmail.com>
2019-01-11 17:57:44 +0100

    Add icons for notes + make header fixed

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,12 +1,3 @@
 $(function() {
   hljs.initHighlighting();
-
-  if(window.innerWidth >= 1024) {
-    if(typeof Waypoint !== "undefined") {
-      var sticky = new Waypoint.Sticky({
-        element: $('#toc')[0],
-        wrapper: false
-      });
-    }
-  }
 });
